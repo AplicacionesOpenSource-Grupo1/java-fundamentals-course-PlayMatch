c55dfc3285908eb18d930457351ea87174814fe5
Mathias Javier Murillo <115802009+K1ngHulk@users.noreply.github.com>
2025-11-14 14:57:39 -0500

    Add files via upload

---
+++ Lesson 2.java
@@ -0,0 +1,11 @@
+int a = 1;
+	int b = 2;
+	
+	int sumar = b + a;
+	System.out.println(sumar);
+
+
+	String nombre = "Juan";
+	int edad = 30;
+	double taman = 2.50;
+	System.out.println("Nombre: " + nombre + "  Edad: " + edad + "  Tamaño: " + taman);
